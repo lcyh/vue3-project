875356c29e768c2ade4f748b738c89889dd1a5c7
luochang <173408798@qq.com>
2021-11-21 12:05:13 +0800

    feat: 添加 打包输出路径

--- dist/js/profile.1eca7757.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["profile"],{"96fe":function(e,n,t){"use strict";t.r(n);var u=t("7a23");n["default"]=Object(u["r"])({setup:function(){return function(){return Object(u["q"])("div",null,[Object(u["q"])("h3",null,[Object(u["p"])("hello jsx")])])}}})}}]);
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,10 @@ const path = require('path')
 
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-project' : '/', // github里 vue3-project项目名,实际的项目一般不需要配置这项
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue3-project' : '/', // github里 vue3-project项目名,实际的项目一般不需要配置这项
   productionSourceMap: false,
+  publicPath: process.env.VUE_APP_BASE_URL,
+  outputDir: process.env.VUE_APP_OUTPUT,
   chainWebpack: (config) => {
     config.resolve.alias.set('@', path.join(__dirname, 'src'))
   },
