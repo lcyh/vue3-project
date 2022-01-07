c5ecc61d5f759f97e7c4a4e86964594cb4dd2872
luochang <173408798@qq.com>
2022-01-07 15:17:16 +0800

    feat: 调整 element-plus 样式文件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 import App from './App.vue'
 import store from '@/store'
-import 'element-plus/dist/index.css'
 
 createApp(App).use(store).use(router).use(ElementPlus)
   .mount('#app')
